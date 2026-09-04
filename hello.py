dbccf5f61ed1e72c6633a3b3eceb7f8e577e03aa
Ayush Mahajan <2605673@sit.singaporetech.edu.sg>
2026-09-04 17:12:50 +0800

    Dynamic Profile

--- hello.py
+++ hello.py
@@ -18,14 +18,25 @@
 #print("Bio:", bio)
 #print("Followers:", followers)
 
+# This is Question 3
+#followers = 100
 
-followers = 100
+#followers += 50
+#print("Day 1:", followers)
 
-followers += 50
-print("Day 1:", followers)
+#followers += 20
+#print("Day 2:", followers)
 
-followers += 20
-print("Day 2:", followers)
+#followers -= 10
+#print("Day 3:", followers)
 
-followers -= 10
-print("Day 3:", followers)
+
+username = input("Enter Username: ")
+age = input("Enter Age: ")
+category = input("Enter Content Category: ")
+
+print("\nInstagram Profile")
+print("====================")
+print("Username:", username)
+print("Age:", age)
+print("Category:", category)
