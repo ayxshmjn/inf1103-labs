1ac97aac79a3573906b7d4085611fe17acfa163b
Ayush Mahajan <2605673@sit.singaporetech.edu.sg>
2026-09-04 17:07:10 +0800

    Assignment Variables

--- hello.py
+++ hello.py
@@ -1,19 +1,31 @@
-
+# This is Activity 1
 #print("=============================")
 #print("Welcome Here")
 #print("My First Post!")
 #print("===============")
 
+# This is Acitivity 2
+#print("=============================")
+#print("Welcome Here")
+#print("My First Post!")
+#print("===============")
 
-print("=============================")
-print("Welcome Here")
-print("My First Post!")
-print("===============")
+#username = "cool creator"
+#bio = "Fun Blogger"
+#followers = 100
 
-username = "cool creator"
-bio = "Fun Blogger"
+#print("Username:", username)
+#print("Bio:", bio)
+#print("Followers:", followers)
+
+
 followers = 100
 
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)
