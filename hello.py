91f8ded979cdeee46147912dd00bc30cd852313f
Ayush Mahajan <2605673@sit.singaporetech.edu.sg>
2026-09-04 17:00:48 +0800

    Added Profile Variables

--- hello.py
+++ hello.py
@@ -1,4 +1,19 @@
+
+#print("=============================")
+#print("Welcome Here")
+#print("My First Post!")
+#print("===============")
+
+
 print("=============================")
 print("Welcome Here")
 print("My First Post!")
 print("===============")
+
+username = "cool creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
